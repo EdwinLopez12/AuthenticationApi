a2f62aec03467974b26b9936af98765da0f75c72
Edwin López <edwin.lopezb.1297@gmail.com>
2021-05-20 12:05:16 -0500

    Browse privileges

---
+++ src/main/java/com/authentication/api/domain/service/PrivilegeService.java
@@ -0,0 +1,26 @@
+package com.authentication.api.domain.service;
+
+import com.authentication.api.infrastructure.persistense.entity.Privilege;
+import com.authentication.api.infrastructure.persistense.jpa.PrivilegeJpaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * The type Privilege service.
+ */
+@Service
+@AllArgsConstructor
+public class PrivilegeService {
+    private final PrivilegeJpaRepository privilegeJpaRepository;
+
+    /**
+     * Get all privileges list.
+     *
+     * @return the list
+     */
+    public List<Privilege> getAllPrivileges(){
+        return privilegeJpaRepository.findAll();
+    }
+}
---
+++ src/main/java/com/authentication/api/infrastructure/controller/PrivilegeController.java
@@ -0,0 +1,34 @@
+package com.authentication.api.infrastructure.controller;
+
+import com.authentication.api.domain.service.PrivilegeService;
+import com.authentication.api.infrastructure.persistense.entity.Privilege;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * The type Privilege controller.
+ */
+@RestController
+@RequestMapping("/api/privileges")
+@AllArgsConstructor
+public class PrivilegeController {
+    private final PrivilegeService privilegeService;
+
+    /**
+     * Get all privileges response entity.
+     *
+     * @return the response entity
+     */
+    @PreAuthorize("hasAuthority('BROWSE_PRIVILEGE')")
+    @GetMapping
+    public ResponseEntity<List<Privilege>> getAllPrivileges(){
+        return new ResponseEntity<>(privilegeService.getAllPrivileges(), HttpStatus.OK);
+    }
+}
