95db5544f7f57c9a5142623f8ebe740cd391cd2c
Edwin López <edwin.lopezb.1297@gmail.com>
2021-05-19 23:00:30 -0500

    Swagger configuration

--- src/main/java/com/authentication/api/AuthenticationApiApplication.java
+++ src/main/java/com/authentication/api/AuthenticationApiApplication.java
@@ -1,13 +1,24 @@
 package com.authentication.api;
 
+import com.authentication.api.app.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * The Authentication api application.
+ */
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class AuthenticationApiApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApiApplication.class, args);
 	}
