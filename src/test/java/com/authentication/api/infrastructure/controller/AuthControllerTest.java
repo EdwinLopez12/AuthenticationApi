70a8e10bfe061bf2069a48ad719a503b907bbb6f
Edwin López <edwin.lopezb.1297@gmail.com>
2021-05-23 14:22:10 -0500

    Test to reset password

--- src/test/java/com/authentication/api/infrastructure/controller/AuthControllerTest.java
+++ src/test/java/com/authentication/api/infrastructure/controller/AuthControllerTest.java
@@ -1,10 +1,9 @@
 package com.authentication.api.infrastructure.controller;
 
-import com.authentication.api.domain.dto.auth.LoginUserRequest;
-import com.authentication.api.domain.dto.auth.LogoutUserRequest;
-import com.authentication.api.domain.dto.auth.RefreshTokenRequest;
-import com.authentication.api.domain.dto.auth.RegisterUserRequest;
+import com.authentication.api.domain.dto.auth.*;
+import com.authentication.api.infrastructure.persistense.entity.PasswordReset;
 import com.authentication.api.infrastructure.persistense.entity.VerificationToken;
+import com.authentication.api.infrastructure.persistense.jpa.PasswordResetJpaRepository;
 import com.authentication.api.infrastructure.persistense.jpa.VerificationTokenJpaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.Instant;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -38,22 +39,28 @@ class AuthControllerTest {
     private static final String USERNAME_REGISTER_TEST = "test";
     private static final String PASSWORD_PLAIN = "123456";
     // signup data invalid
-    private static final String EMAIL_REGISTER_TEST_INVALID = "@mail.com";
-    private static final String USERNAME_REGISTER_TEST_INVALID = "tt";
+    private static final String EMAIL_INVALID = "@mail.com";
+    private static final String USERNAME_INVALID = "tt";
     private static final String PASSWORD_INVALID = "1234";
     // Login no exist
     private static final String USERNAME_NOT_EXIST = "otherUser";
+    private static final String EMAIL_NOT_EXIST = "otherUser@mail.com";
     // user data with user privileges
     private static final String USERNAME_BASIC_PRIVILEGES = "USER";
     private static final String EMAIL_USER_PRIVILEGES = "USER@mail.com";
     private static final String REFRESH_TOKEN_USER_PRIVILEGES = "e8818837-d76c-4c95-bc72-edc9fdcc693d-USER";
     private static final String VERIFICATION_TOKEN_USER_PRIVILEGES = "2d5eff8a-16c0-46f7-a66f-9aebe0c0388d-USER";
+    // user data with admin privileges
+    private static final String USERNAME_ADMIN_PRIVILEGES = "ADMIN";
+    private static final String EMAIL_ADMIN_PRIVILEGES ="ADMIN@mail.com";
 
     private static final String URL_AUTH = "http://localhost:9090/api/auth/";
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private VerificationTokenJpaRepository verificationTokenJPARepository;
+    @Autowired
+    private PasswordResetJpaRepository passwordResetJpaRepository;
     @Autowired
     private WebApplicationContext webApplicationContext;
     @Autowired
@@ -78,9 +85,9 @@ class AuthControllerTest {
     @Test
     void signup_return_status_unprocessable_entity_if_data_is_invalid() throws Exception{
         RegisterUserRequest registerUserRequest = RegisterUserRequest.builder()
-                .username(USERNAME_REGISTER_TEST_INVALID)
+                .username(USERNAME_INVALID)
                 .password(PASSWORD_INVALID)
-                .email(EMAIL_REGISTER_TEST_INVALID)
+                .email(EMAIL_INVALID)
                 .build();
 
         String postValue = objectMapper.writeValueAsString(registerUserRequest);
@@ -214,7 +221,7 @@ class AuthControllerTest {
     @Test
     void login_return_status_unprocessable_entity_if_data_is_invalid() throws Exception{
         LoginUserRequest loginUserRequest = LoginUserRequest.builder()
-                .username(USERNAME_REGISTER_TEST_INVALID)
+                .username(USERNAME_INVALID)
                 .password(PASSWORD_INVALID)
                 .build();
 
@@ -336,8 +343,8 @@ class AuthControllerTest {
 
         Integer body = mockMvc.perform(post(URL_AUTH+ "refresh/token")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(postValue)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(postValue))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getStatus();
         assertEquals(200, body);
@@ -358,8 +365,8 @@ class AuthControllerTest {
 
         MockHttpServletResponse response = mockMvc.perform(post(URL_AUTH+ "logout")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(postValue)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(postValue))
                 .andExpect(status().isInternalServerError())
                 .andReturn().getResponse();
         assertEquals(500, response.getStatus());
@@ -380,12 +387,221 @@ class AuthControllerTest {
 
         MockHttpServletResponse response = mockMvc.perform(post(URL_AUTH+ "logout")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(postValue)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(postValue))
                 .andExpect(content().string("Refresh token deleted successfully"))
                 .andExpect(status().isOk())
                 .andReturn().getResponse();
         assertEquals("Refresh token deleted successfully", response.getContentAsString());
         assertEquals(200, response.getStatus());
+    }
+
+    /**
+     * Send email reset password throw exception if email is invalid.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void send_email_reset_password_throw_exception_if_email_is_invalid() throws Exception {
+        EmailPasswordResetRequest emailPasswordResetRequest = EmailPasswordResetRequest.builder()
+                .email(EMAIL_INVALID)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(emailPasswordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(post(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+
+                .andExpect(status().isUnprocessableEntity())
+                .andReturn().getResponse();
+        assertEquals(422, response.getStatus());
+    }
+
+    /**
+     * Send email reset password throw exception if email no exist.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void send_email_reset_password_throw_exception_if_email_no_exist() throws Exception {
+        EmailPasswordResetRequest emailPasswordResetRequest = EmailPasswordResetRequest.builder()
+                .email(EMAIL_NOT_EXIST)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(emailPasswordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(post(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(status().isNotFound())
+                .andReturn().getResponse();
+        assertEquals(404, response.getStatus());
+    }
+
+    /**
+     * Send email reset password if data is valid and email exist.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void send_email_reset_password_if_data_is_valid_and_email_exist() throws Exception {
+        EmailPasswordResetRequest emailPasswordResetRequest = EmailPasswordResetRequest.builder()
+                .email(EMAIL_USER_PRIVILEGES)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(emailPasswordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(post(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(content().string("Email was send"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+        assertEquals(200, response.getStatus());
+        assertEquals("Email was send", response.getContentAsString());
+    }
+
+    /**
+     * Reset password verify token throw exception if token no exist.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void reset_password_verify_token_throw_exception_if_token_no_exist() throws Exception {
+        String token = UUID.randomUUID().toString();
+
+        MockHttpServletResponse response = mockMvc.perform(get(URL_AUTH+"reset/password/verification/"+token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError())
+                .andReturn().getResponse();
+        assertEquals(500, response.getStatus());
+    }
+
+    /**
+     * Reset password verify token if token exist.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void reset_password_verify_token_if_token_exist() throws Exception {
+        String token = UUID.randomUUID().toString();
+        PasswordReset passwordReset = PasswordReset.builder()
+                .email(EMAIL_USER_PRIVILEGES)
+                .token(token)
+                .date(Instant.now())
+                .build();
+        passwordResetJpaRepository.save(passwordReset);
+
+        MockHttpServletResponse response = mockMvc.perform(get(URL_AUTH+"reset/password/verification/"+token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("Token verified"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+        assertEquals(200, response.getStatus());
+        assertEquals("Token verified", response.getContentAsString());
+    }
+
+    /**
+     * Update password throw exception if data is invalid.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void update_password_throw_exception_if_data_is_invalid() throws Exception {
+        PasswordResetRequest passwordResetRequest = PasswordResetRequest.builder()
+                .email(EMAIL_INVALID)
+                .password(PASSWORD_INVALID)
+                .passwordVerify(PASSWORD_INVALID)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(passwordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(put(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(status().isUnprocessableEntity())
+                .andReturn().getResponse();
+        assertEquals(422, response.getStatus());
+    }
+
+    /**
+     * Update password throw exception if user not found.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void update_password_throw_exception_if_user_not_found() throws Exception {
+        PasswordResetRequest passwordResetRequest = PasswordResetRequest.builder()
+                .email(EMAIL_NOT_EXIST)
+                .password(PASSWORD_PLAIN)
+                .passwordVerify(PASSWORD_PLAIN)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(passwordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(put(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(status().isNotFound())
+                .andReturn().getResponse();
+        assertEquals(404, response.getStatus());
+    }
+
+    /**
+     * Update password throw exception if passwords not match.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void update_password_throw_exception_if_passwords_not_match() throws Exception {
+        PasswordResetRequest passwordResetRequest = PasswordResetRequest.builder()
+                .email(EMAIL_ADMIN_PRIVILEGES)
+                .password(PASSWORD_PLAIN+"-")
+                .passwordVerify(PASSWORD_PLAIN)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(passwordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(put(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(status().isConflict())
+                .andReturn().getResponse();
+        assertEquals(409, response.getStatus());
+    }
+
+    /**
+     * Update password if data is valid.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    void update_password_if_data_is_valid() throws Exception {
+        PasswordResetRequest passwordResetRequest = PasswordResetRequest.builder()
+                .email(EMAIL_ADMIN_PRIVILEGES)
+                .password(PASSWORD_PLAIN)
+                .passwordVerify(PASSWORD_PLAIN)
+                .build();
+
+        String postValue = objectMapper.writeValueAsString(passwordResetRequest);
+
+        MockHttpServletResponse response = mockMvc.perform(put(URL_AUTH+"reset/password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(postValue))
+                .andExpect(content().string("Password reset successfully"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+        assertEquals(200, response.getStatus());
+        assertEquals("Password reset successfully", response.getContentAsString());
     }
 }
