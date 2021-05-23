c505f3c3e9850d696b14ef8834bcffef5e2e082f
Edwin López <edwin.lopezb.1297@gmail.com>
2021-05-22 23:01:25 -0500

    Entity and repository for password reset

---
+++ src/main/java/com/authentication/api/infrastructure/persistense/jpa/PasswordResetJpaRepository.java
@@ -0,0 +1,29 @@
+package com.authentication.api.infrastructure.persistense.jpa;
+
+import com.authentication.api.infrastructure.persistense.entity.PasswordReset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * The interface Password reset jpa repository.
+ */
+@Repository
+public interface PasswordResetJpaRepository extends JpaRepository<PasswordReset, Long> {
+
+    /**
+     * Find by token optional.
+     *
+     * @param token the token
+     * @return the optional
+     */
+    Optional<PasswordReset> findByToken(String token);
+
+    /**
+     * Delete by email.
+     *
+     * @param email the email
+     */
+    void deleteByEmail(String email);
+}
